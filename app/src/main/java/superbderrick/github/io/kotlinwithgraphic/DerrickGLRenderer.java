91b93f0ed4e087d10d7d7ac2a878b2bc1a11dc94
derrick_kang <derrick.kang@unity3d.com>
2019-07-24 18:05:59 +0900

    added first commit

---
+++ app/src/main/java/superbderrick/github/io/kotlinwithgraphic/DerrickGLRenderer.java
@@ -0,0 +1,23 @@
+package superbderrick.github.io.kotlinwithgraphic;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class DerrickGLRenderer implements GLSurfaceView.Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+
+    }
+}
